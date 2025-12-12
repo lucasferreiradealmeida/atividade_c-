6f4a2d90cfa804fc259c4cd0ace53f9ff0e6f901
Lucas Ferreira de Almeida <lucasfalmeida07@gmail.com>
2025-12-12 13:25:25 -0300

    dia 4

---
+++ atividades/exercicio2.py
@@ -0,0 +1,7 @@
+nome = 'Lucas Ferreira'
+altura = 1.73
+peso = 70
+imc = peso / (altura * altura)
+print(nome, 'tem', altura, 'de altura,')
+print('pesa', peso, 'quilos e seu IMC é:')
+print(imc)
---
+++ atividades/exercicio3.py
@@ -0,0 +1,10 @@
+primeiro_valor = input('Digite um valor:')
+segundo_valor = input('Digite outro valor:')
+if primeiro_valor > segundo_valor:
+    print(f'{primeiro_valor=} é maior do que o {segundo_valor=}')
+elif primeiro_valor < segundo_valor:
+    print(f'{segundo_valor=} é maior do que o {primeiro_valor=}')
+elif primeiro_valor == segundo_valor:
+    print(f'{segundo_valor=} é igual ao {primeiro_valor=}')
+else:
+    print(f'voce digitou algo incorreto')
